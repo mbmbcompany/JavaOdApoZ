5eed7081bc12e171adce00d8cf5ca9f61ef7da3d
Martin Bielik <martinbielik@Martins-MacBook-Pro.local>
2020-10-28 12:08:15 +0100

    0.1

---
+++ src/main/java/sk/luigicocktails/luigicockails/LuigicockailsApplication.java
@@ -0,0 +1,13 @@
+package sk.luigicocktails.luigicockails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuigicockailsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuigicockailsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sk/luigicocktails/luigicockails/LuigicockailsApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.luigicocktails.luigicockails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuigicockailsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
